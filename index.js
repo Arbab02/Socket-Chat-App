53e57341d69ce96443305c08577e86ff03d226db
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2024-01-06 03:20:52 -0800

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ const io = socketIo(server);
 
 app.use(express.static(__dirname + '/public'));
 
+const messageHistory = []; // Store message history
+
 io.on('connection', (socket) => {
   console.log('A user connected');
 
+  // Send message history to new user
+  socket.emit('message history', messageHistory);
+
   socket.on('chat message', (msg) => {
+    messageHistory.push(msg); // Save the message to history
     io.emit('chat message', msg);
   });
 
